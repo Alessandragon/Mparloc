34a004b46738b30d58f72fde2458f092410bb571
Alessndro Caruso <alessandro.caruso@unina.it>
2021-02-04 10:01:27 +0100

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 #IMPORT OF LIBRERY
-from MParLoc import Luncher 
+from MParLoc import Launcher 
 from MParLoc import grid_creator
 ##############################
 grid_creator('EXAMPLE','sac') 
 # RUN THE GRID CREATOR CODE THAT READ THE NONLINLOC GRIDS 
 #AND CREATE A DIFFERENTIAL AMPLITUDE AND BACK-AZIMUT GRIDS
-Luncher('EXAMPLE','sac')
+Launcher('EXAMPLE','sac')
 # RUN THE GRID CREATOR CODE THAT READ THE NONLINLOC GRIDS 
 #AND CREATE A DIFFERENTIAL AMPLITUDE AND BACK-AZIMUT GRIDS
